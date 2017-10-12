fda8787276820f7eb97ab62370eca7c0c8819da7
ADMIN <ADMIN@DESKTOP-MKK35AC>
2017-10-12 04:31:35 -0700

    code for gmail and talentify login

--- src/test/demo1.java
+++ src/test/demo1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class demo {
+public class demo1 {
  public static void main(String args[]) throws InterruptedException{
 	 System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
 	 WebDriver driver=new ChromeDriver();
--- src/test/gmail1.java
+++ src/test/gmail1.java
@@ -1,18 +1,22 @@
 package test;
 
-public class gmail {
-	package com.example.tests;
+public class gmail1 {
 
-	import java.util.regex.Pattern;
-	import java.util.concurrent.TimeUnit;
-	import org.junit.*;
-	import static org.junit.Assert.*;
-	import static org.hamcrest.CoreMatchers.*;
-	import org.openqa.selenium.*;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.openqa.selenium.support.ui.Select;
 
-	public class Gmail {
+	import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 	  private WebDriver driver;
 	  private String baseUrl;
 	  private boolean acceptNextAlert = true;
